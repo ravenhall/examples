f107a62e866f787a21c09a974b5f1e14482845e3
Nathan Waddell - Contractor <nathan.waddell.contractor@hilton.com>
2018-04-12 12:20:25 -0500

    reformatted with Perl::Tidy

--- perl/processtree.pl
+++ perl/processtree.pl
@@ -25,13 +25,9 @@ processtree.pl
 Nathan Waddell <nathan.e.waddell@gmail.com>
 
 =cut
-
 my $help;
 my $debug;
-GetOptions(
-    'debug'  => \$debug,
-    'help|?' => \&_usage,
-);
+GetOptions('debug' => \$debug, 'help|?' => \&_usage,);
 
 # create a hash for holding our data
 my %table = ();
@@ -40,10 +36,9 @@ my %table = ();
 _build_tree();
 
 # print our tree
-foreach my $parent ( sort { $a <=> $b } keys %table ) {
-    _print_tree( $table{$parent} );
+foreach my $parent (sort { $a <=> $b } keys %table) {
+  _print_tree($table{$parent});
 }
-
 exit;
 
 =head2 Subroutines
@@ -67,13 +62,13 @@ None
 =cut
 
 sub _usage {
-    my $name = basename($0);
-    say "$name";
-    say "Usage: $name [OPTIONS]";
-    say "\t Options:";
-    say "\t\t -d --debug" . "\t" . "Turn on output debugging";
-    say "\t\t -h -? --help" . "\t" . "Show this usage message";
-    exit;
+  my $name = basename($0);
+  say "$name";
+  say "Usage: $name [OPTIONS]";
+  say "\t Options:";
+  say "\t\t -d --debug" . "\t" . "Turn on output debugging";
+  say "\t\t -h -? --help" . "\t" . "Show this usage message";
+  exit;
 }
 
 =head2 _build_tree
@@ -96,46 +91,43 @@ None
 
 sub _build_tree {
 
-    # our data input comes from this command
-    my @array = qx/ps -e l/;
-    exit "Error: No output received from command: ps -e l" unless @array;
+  # our data input comes from this command
+  my @array = qx/ps -e l/;
+  exit "Error: No output received from command: ps -e l" unless @array;
 
-    # get our column headers
-    my @fields = split /\s+/, shift @array;
+  # get our column headers
+  my @fields = split /\s+/, shift @array;
+  foreach my $line (@array) {
+    chomp $line;
 
-    foreach my $line (@array) {
-        chomp $line;
+    # get the data from the line into an array, limit by number of fields
+    my @data = split /\s+/, $line, scalar(@fields);
 
-        # get the data from the line into an array, limit by number of fields
-        my @data = split /\s+/, $line, scalar(@fields);
+    # create a hash for holding this line's data
+    my %linedata;
 
-        # create a hash for holding this line's data
-        my %linedata;
+    # combine our keys and values
+    @linedata{@fields} = @data;
 
-        # combine our keys and values
-        @linedata{@fields} = @data;
+    # ease of use
+    my $pid = $linedata{PID};
 
-        # ease of use
-        my $pid = $linedata{PID};
+    # get the process name separate from its args
+    my ($name, $args) = split /\s+/, $linedata{COMMAND}, 2;
 
-        # get the process name separate from its args
-        my ( $name, $args ) = split /\s+/, $linedata{COMMAND}, 2;
+    # store it in our line's hash
+    $linedata{NAME} = $name;
 
-        # store it in our line's hash
-        $linedata{NAME} = $name;
-
-        # if the PPID is 0 there's no need to search for a father, so store it.
-        if ( $linedata{PPID} == 0 ) {
-            $table{$pid} = \%linedata;
-            next;
-        }
-
-        foreach my $proc ( sort { $a <=> $b } keys %table ) {
-            my $entry = $table{$proc};
-            _find_father( $entry, \%linedata );
-        }
-
+    # if the PPID is 0 there's no need to search for a father, so store it.
+    if ($linedata{PPID} == 0) {
+      $table{$pid} = \%linedata;
+      next;
     }
+    foreach my $proc (sort { $a <=> $b } keys %table) {
+      my $entry = $table{$proc};
+      _find_father($entry, \%linedata);
+    }
+  }
 }
 
 =head2 _find_father
@@ -158,29 +150,25 @@ True if parent was found, false otherwise.
 =cut
 
 sub _find_father {
-    my $process = shift;
-    my $child   = shift;
-
-    return unless $process;
-    return unless $child;
-
-    my $found = 0;
-    if ( $process->{PID} == $child->{PPID} ) {
-        unless ( $process->{CHILD} ) {
-            $process->{CHILD} = [$child];
-        }
-        else {
-            push @{ $process->{CHILD} }, $child;
-        }
-        $found = 1;
+  my $process = shift;
+  my $child   = shift;
+  return unless $process;
+  return unless $child;
+  my $found = 0;
+  if ($process->{PID} == $child->{PPID}) {
+    unless ($process->{CHILD}) {
+      $process->{CHILD} = [$child];
+    } else {
+      push @{$process->{CHILD}}, $child;
     }
-    elsif ( $process->{CHILD} && !$found ) {
-        my @kids = @{ $process->{CHILD} };
-        foreach my $kid (@kids) {
-            $found = _find_father( $kid, $child ) unless $found;
-        }
+    $found = 1;
+  } elsif ($process->{CHILD} && !$found) {
+    my @kids = @{$process->{CHILD}};
+    foreach my $kid (@kids) {
+      $found = _find_father($kid, $child) unless $found;
     }
-    return $found;
+  }
+  return $found;
 }
 
 =head2 _print_tree
@@ -202,23 +190,19 @@ None
 =cut
 
 sub _print_tree {
-    my $process = shift;
-    my $indent  = shift;
-    return unless $process;
+  my $process = shift;
+  my $indent  = shift;
+  return unless $process;
+  my $sep = " ";
+  $sep = "    " x $indent . '\_' if $indent;
+  my $line = $process->{PID} . $sep . $process->{NAME};
+  $line .= "\t" . "Parent:" . $process->{PPID} if $debug;
+  say $line;
 
-    my $sep = " ";
-    $sep = "    " x $indent . '\_' if $indent;
-
-    my $line = $process->{PID} . $sep . $process->{NAME};
-
-    $line .= "\t" . "Parent:" . $process->{PPID} if $debug;
-
-    say $line;
-
-    if ( $process->{CHILD} ) {
-        $indent++;
-        foreach my $child ( @{ $process->{CHILD} } ) {
-            _print_tree( $child, $indent );
-        }
+  if ($process->{CHILD}) {
+    $indent++;
+    foreach my $child (@{$process->{CHILD}}) {
+      _print_tree($child, $indent);
     }
+  }
 }
